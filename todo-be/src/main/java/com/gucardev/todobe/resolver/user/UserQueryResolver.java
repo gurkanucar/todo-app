c151b65d545da7bcf981c2fc64bef797f56f4010
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-02-10 17:34:12 +0300

    user query resolver implemented

---
+++ todo-be/src/main/java/com/gucardev/todobe/resolver/user/UserQueryResolver.java
@@ -0,0 +1,22 @@
+package com.gucardev.todobe.resolver.user;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.gucardev.todobe.dto.UserDto;
+import com.gucardev.todobe.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserQueryResolver implements GraphQLQueryResolver {
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+
+    public UserDto getUser(Long id) {
+        return modelMapper.map(userService.getUserByID(id), UserDto.class);
+    }
+
+}
