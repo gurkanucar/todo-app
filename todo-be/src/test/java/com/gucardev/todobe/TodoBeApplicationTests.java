ab76f746d28d0654105a9cfecd9c083f82c4bdf5
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-02-10 17:17:59 +0300

    projects created

---
+++ todo-be/src/test/java/com/gucardev/todobe/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.todobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
