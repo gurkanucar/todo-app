96f1bc8414146cb52e505492100ac605c1b39a3a
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-02-10 17:29:22 +0300

    user service created

---
+++ todo-be/src/main/java/com/gucardev/todobe/service/UserService.java
@@ -0,0 +1,27 @@
+package com.gucardev.todobe.service;
+
+import com.gucardev.todobe.model.User;
+import com.gucardev.todobe.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserByID(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("user not found!"));
+    }
+
+    void checkUser(Long id) {
+        getUserByID(id);
+    }
+
+}
