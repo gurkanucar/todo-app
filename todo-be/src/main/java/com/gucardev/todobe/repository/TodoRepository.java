2d35b3eb182dca19642534b7408bc30f4f5ec2a5
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-02-10 17:31:04 +0300

    custom queries added to todo repository

--- todo-be/src/main/java/com/gucardev/todobe/repository/TodoRepository.java
+++ todo-be/src/main/java/com/gucardev/todobe/repository/TodoRepository.java
@@ -4,7 +4,13 @@ import com.gucardev.todobe.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo,Long> {
+
+    List<Todo> findAllByUserId(Long userID);
+
+    List<Todo> findAllByUserIdAndCompleted(Long userID, Boolean completed);
 
 }
